cd2605edb5b94a8e54ba1d8d3b7963af25b4ad2a
Volodia Rybas <rybasvolodia@gmail.com>
2022-01-05 13:52:02 +0200

    added test for first reverse

--- First-Reverse.js
+++ First-Reverse.js
@@ -1,1 +1,10 @@
+const assert = require('assert');
+
 const StringChallenge = (string) => string.split('').reverse().join('');
+
+assert.equal(StringChallenge('Hello'), 'olleH');
+assert.equal(StringChallenge('a'), 'a');
+assert.equal(StringChallenge('az'), 'za');
+assert.equal(StringChallenge('test'), 'tset');
+assert.equal(StringChallenge('coderbyte'), 'etybredoc');
+assert.equal(StringChallenge('I Love Code'), 'edoC evoL I');
