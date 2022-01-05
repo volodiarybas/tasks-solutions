1d5c79c385278290f0945df18267419db5413fa6
Volodia Rybas <rybasvolodia@gmail.com>
2022-01-05 16:07:27 +0200

    fix simple symbols

--- Simple-Symbols.js
+++ Simple-Symbols.js
@@ -1,12 +1,11 @@
 const assert = require('assert');
 
-const StringChallenge = (string) => string.split(/[a-z]|[0-9]/g).every(letterSurroundings => letterSurroundings.startsWith('+') 
+const StringChallenge = (string) => string.split(/[a-z]/g).every(letterSurroundings => letterSurroundings.startsWith('+') 
     && letterSurroundings.endsWith('+') 
-    || letterSurroundings.length === 0
 );
 
 assert.equal(StringChallenge('++d+===+c++==a'), false);
 assert.equal(StringChallenge('+a+'), true);
-assert.equal(StringChallenge('a+'), true);
-assert.equal(StringChallenge('+d+=3=+s+'), false);
-assert.equal(StringChallenge('f++d+'), true);
+assert.equal(StringChallenge('a+'), false);
+assert.equal(StringChallenge('+d+=3=+s+'), true);
+assert.equal(StringChallenge('f++d+'), false);
