3e07ec56463a5094581dfa3418126eafd0ba4a46
Volodia Rybas <rybasvolodia@gmail.com>
2022-01-05 13:46:38 +0200

    added tests for recursion task

---
+++ First-Recursion.js
@@ -0,0 +1,15 @@
+const assert = require('assert');
+
+const RecursionChallenge = num => {
+	if (num === 1) return num;
+  return num * RecursionChallenge(num - 1);
+};
+
+assert.equal(RecursionChallenge(1), 1);
+assert.equal(RecursionChallenge(3), 6);
+assert.equal(RecursionChallenge(4), 24);
+assert.equal(RecursionChallenge(8), 40320);
+
+
+
+
